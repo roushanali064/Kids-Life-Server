b1827791b4dda79317c3703d85f7c5e574da0def
roushanali064 <roushanrhaman064@gmail.com>
2023-05-21 19:46:00 +0600

    toy id serch done

--- index.js
+++ index.js
@@ -72,6 +72,14 @@ async function run() {
       res.send(result)
     })
 
+    app.get('/toy/:id', async(req,res)=>{
+      const id = req.params.id;
+      console.log(id)
+      const query = {_id: new ObjectId(id)}
+      const result = await kidsLifeCollection.findOne(query)
+      res.send(result)
+    })
+
     app.delete('/toys/:id', async(req,res)=>{
       const id = req.params.id;
       
